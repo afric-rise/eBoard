1106ac338b086bc1f9be28fa57b78490d98fc979
Savio Sylivester Kalinga <83492030+afric-rise@users.noreply.github.com>
2023-08-15 20:22:31 +0300

    Delete lib directory

--- lib/src/admin/constants/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const eBoardBlue = Color(0xFF4153B5);
-const eBoardGrey = Color(0xFFF1F0E8);
-const eBoardWhite = Color(0xFFffffff);
-const eBoardBlack = Color(0xFF000000);
-const eBoardYellow = Color(0xFFFF9800);
