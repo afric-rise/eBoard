b776fce5011216554b19c14cc0a94d42ba39ff4c
Savio Sylivester Kalinga <83492030+afric-rise@users.noreply.github.com>
2023-08-15 21:03:15 +0300

    Add files via upload

--- lib/src/admin/constants/texts.dart
+++ lib/src/admin/constants/texts.dart
@@ -1,8 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+
 //Images
 const String tcraLogo = "assets/images/TCRAlogo.png";
 const String tpaLogo = "assets/images/tpa.png";
 const String egaLogo = "assets/images/logo.png";
 const String bibiNaBwana = "assets/images/nembo.png";
+const String board = "assets/images/board.png";
+const String committee = "assets/images/cometee.png";
 
 // Dash board Text
 const String eLogoText = "eBoard";
@@ -20,25 +26,52 @@ const String eTitleThree = "EGA Management";
 const String eManagement = "Management";
 const String eCommittee = "Committee";
 const String eComm = "COMMI";
+const String edash = "-";
+const String eSpace = " ";
 
-//drawer
-const String userDashboard = "Dashboard";
-const String initialTitle = "Dashboard";
-const String meetingSchedule = "Meeting and Schedule";
-const String boardCalender = "Board calender";
-const String directives = "Directives";
-const String voting = "Voting";
-const String decisionRegister = "Decision Register";
-const String packsAnnotations = "Packs and Annotations";
-const String circularResolution = "Circular Resolution";
-const String governmentRepository = "Government Repository";
-const String boardStructure = "Board Structure";
-const String auditTrail = "Audit Trail";
-const String boardReports = "Board Reports";
-const String ucsaf = "UCSAF";
-const String bodiYaWkurugenziUCSAF = "Bodi ya wakurugenzi UCSAF";
 
-//dashboard tab bar
-const String meetingAndResolutions = "Meeting and Resolutions";
-const String attendees = "Attendees";
-const String news = "News and Updates";
+//Meetings Text
+const String eMeetings = "Board Meetings";
+const String eMeetiingsTitle = "Bodi ya Wakurugenzi - eGA";
+const String eMonth = "Dec";
+const String eDate = "10";
+const String eDay = "Saturday";
+const String eTime = "12:30";
+const String eTimeZone = "(UTC+03:00)";
+const String eLocation = "Mtumba";
+const String eDescription = "Register New Meeting.\n I am editing this\n meeting now";
+const String eCommiteeHead = "Committees Meetings";
+const String eCommittesTitle = "AIC(eGA)";
+const String eCommiteeMonth = "Mar";
+const String eCommiteeDate = "08";
+const String eCommiteeDay = "Wednesday";
+const String eCommiteeTime = "08:40";
+const String eCommiteeTimezone = "(Africa/Casablanca)";
+const String eCommiteeDescription = "This is new Title";
+
+//MESSAGE DETAILS
+const String eInputOne = "Select Agenda Item from list of Meeting Agenda";
+const String eInputOneOptionOne = "First meeting Agenda";
+const String eInputOneOptinonTwo = "Confirm minutes from previous meeting";
+const String eInputOneOptionThree = "Vote";
+const String eInputTwo = "Nature of Interest";
+const String eConflict = " Declare Conflict of Interest";
+const String eOpen = "Open";
+const String eDeclare = "Declare Conflict \nof Interest";
+const String eView = "View Conflict\n of Interest";
+const String ePreview = "Preview";
+const String eDetailsDate = "Date:";
+const String eDetailsTime = "Time:";
+const String eDetailsLocation = "Location:";
+const String eDetailsVideo = "Video URL:";
+const String eDetailsOrganizer = "Meeting Organiser:";
+const String eDetailsQuorum = "Meeting Quorum:";
+const String eDetailsAttendance = "Attendance:";
+const String eDetailsInvitees = "Invitees:";
+const String eDetailsNotes = "Notes:";
+const String eDetailsDatevalue = "December 10, 2022";
+const String eDetailsTimeValue = "12:01 - 13:05";
+const String eDetailsLocationValue = "eGA-DarDodoma, \nMji wa Serikali Mtumba\nUTC+03:00";
+const String eDetailsOrganizerValue = "Jackson Bakari";
+const String eDetailsQuorumValue = " Need More than 50% of\n Board Members to Confirm\n Attendance";
+const String eAttended = "Paul Pantaleo\n Edward Suta\n Vanessa Mhando";
